1678e1a6abdf21ba040f45855361bacc714ca919
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-03-09 12:00:27 -0800

    handle if no domain in request.js

--- utils/request.js
+++ utils/request.js
@@ -1,6 +1,13 @@
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
+
 async function fetchProperties() {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+    // Handle if domain is unavailable
+    if (!apiDomain) {
+      return [];
+    }
+
+    const res = await fetch(`${apiDomain}/properties`);
 
     if (!res.ok) {
       throw new Error('Failed To Fetch Data');
@@ -9,6 +16,7 @@ async function fetchProperties() {
     return res.json();
   } catch (error) {
     console.log(error);
+    return [];
   }
 }
 
