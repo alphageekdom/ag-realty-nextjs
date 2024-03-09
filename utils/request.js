c8194faff9fc6e56de25e77fadf35fe01d883c43
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-03-09 12:53:34 -0800

    Fetch single property data for page

--- utils/request.js
+++ utils/request.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+// Fetch All Properties
 async function fetchProperties() {
   try {
     // Handle if domain is unavailable
@@ -20,4 +21,25 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+// Fetch Single Property
+async function fetchProperty(id) {
+  try {
+    // Handle if domain is unavailable
+    if (!apiDomain) {
+      return null;
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error('Failed To Fetch Data');
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
