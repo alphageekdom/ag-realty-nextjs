de7abddf277398cbf27c413183a64071c73423bf
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-03-09 08:59:05 -0800

    User Schema

---
+++ models/User.js
@@ -0,0 +1,31 @@
+import { Schema, model, models } from 'mongoose';
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: [true, 'Email Already Exists'],
+      required: [true, 'Email Is Required'],
+    },
+    username: {
+      type: String,
+      required: [true, 'Username Is Required'],
+    },
+    image: {
+      type: String,
+    },
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Property',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = models.User || model('User', UserSchema);
+
+export default User;
