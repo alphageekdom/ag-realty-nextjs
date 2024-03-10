0c8535613dac206fd7dccc96421b71c945dd604a
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-03-10 14:08:06 -0700

    Protect routes and auto close menu dropdown

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -202,6 +202,9 @@ const Navbar = () => {
                       role='menuitem'
                       tabIndex='-1'
                       id='user-menu-item-0'
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                      }}
                     >
                       Your Profile
                     </Link>
@@ -211,6 +214,9 @@ const Navbar = () => {
                       role='menuitem'
                       tabIndex='-1'
                       id='user-menu-item-2'
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                      }}
                     >
                       Saved Properties
                     </Link>
