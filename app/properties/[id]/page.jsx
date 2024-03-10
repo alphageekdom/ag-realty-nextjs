806194818d24a35049c8efab9a27907d862bd60f
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-03-09 17:01:11 -0800

    Spinner Component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -7,6 +7,7 @@ import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 import Link from 'next/link';
 import PropertyDetails from '@/components/PropertyDetails';
 import { FaArrowLeft } from 'react-icons/fa';
+import Spinner from '@/components/Spinner';
 
 const PropertyPage = () => {
   const { id } = useParams();
@@ -43,6 +44,7 @@ const PropertyPage = () => {
 
   return (
     <>
+      {loading && <Spinner loading={loading} />}
       {!loading && property && (
         <>
           <PropertyHeaderImage image={property.images[0]} />
