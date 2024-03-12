9ee1a1d7ec6cf8d3731a771f692a5ebc223bde47
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-03-11 18:39:02 -0700

    Add Property POST API Route

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -18,3 +18,52 @@ export const GET = async (request) => {
     });
   }
 };
+
+export const POST = async (request) => {
+  try {
+    const formData = await request.formData();
+
+    // Access all values from amenities and images
+    const amenities = formData.getAll('amenities');
+    const images = formData
+      .getAll('images')
+      .filter((image) => image.name !== '');
+
+    // Create propertyData Object for database
+    const propertyData = {
+      type: formData.get('type'),
+      name: formData.get('name'),
+      description: formData.get('description'),
+      location: {
+        street: formData.get('location.street'),
+        city: formData.get('location.street'),
+        state: formData.get('location.state'),
+        zipcode: formData.get('location.zipcode'),
+      },
+      beds: formData.get('beds'),
+      baths: formData.get('baths'),
+      square_feet: formData.get('square_feet'),
+      amenities,
+      rates: {
+        weekly: formData.get('rates.weekly'),
+        monthly: formData.get('rates.monthly'),
+        nightly: formData.get('rates.nightly'),
+      },
+      seller_info: {
+        name: formData.get('seller_info.name'),
+        email: formData.get('seller_info.email'),
+        phone: formData.get('seller_info.phone'),
+      },
+      images,
+    };
+
+    console.log(propertyData);
+
+    return new Response(JSON.stringify({ message: 'Success' }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response('Failed To Add Property', { status: 500 });
+  }
+};
