60f9a719d7ca771998cb9b570aec796639989dbd
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-03-12 18:52:08 -0700

    Geocoding & Mapbox Map

--- components/PropertyEditForm.jsx
+++ components/PropertyEditForm.jsx
@@ -629,7 +629,7 @@ const PropertyEditForm = () => {
 
         <div>
           <button
-            className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline'
+            className='bg-cyan-600 hover:bg-cyan-500 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline'
             type='submit'
           >
             Update Property
