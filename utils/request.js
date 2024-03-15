b322e921872841b22a4878c4e3f3a659c61fd201
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-03-15 12:42:42 -0700

    Fetch Featured Properties component

--- utils/request.js
+++ utils/request.js
@@ -1,14 +1,17 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
 // Fetch All Properties
-async function fetchProperties() {
+async function fetchProperties({ showFeatured = false } = {}) {
   try {
     // Handle if domain is unavailable
     if (!apiDomain) {
       return [];
     }
 
-    const res = await fetch(`${apiDomain}/properties`, { cache: 'no-store' });
+    const res = await fetch(
+      `${apiDomain}/properties${showFeatured ? '/featured' : ''}`,
+      { cache: 'no-store' }
+    );
 
     if (!res.ok) {
       throw new Error('Failed To Fetch Data');
