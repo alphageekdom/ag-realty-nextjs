95638d642f3e6ed38f9a829e03dadbc2bd06cc2e
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-05-16 20:14:14 -0700

    changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,12 +12,11 @@ import UnreadMessageCount from './UnreadMessageCount';
 
 const Navbar = () => {
   const { data: session, status } = useSession();
-
   const profileImage = session?.user?.image;
 
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
-  const [providers, setProviders] = useState(false);
+  const [providers, setProviders] = useState(null);
 
   const pathname = usePathname();
 
@@ -27,13 +26,14 @@ const Navbar = () => {
       setProviders(res);
     };
 
+    setAuthProviders();
+
     const handleResize = () => {
       if (window.innerWidth >= 768) {
         setIsMobileMenuOpen(false);
       }
     };
 
-    setAuthProviders();
     window.addEventListener('resize', handleResize);
 
     return () => {
@@ -51,7 +51,7 @@ const Navbar = () => {
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
         <div className='relative flex h-20 items-center justify-between'>
           <div className='absolute inset-y-0 left-0 flex items-center md:hidden'>
-            {/* <!-- Mobile menu button--> */}
+            {/* Mobile menu button */}
             <button
               type='button'
               id='mobile-dropdown-button'
@@ -80,7 +80,7 @@ const Navbar = () => {
           </div>
 
           <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
-            {/* <!-- Logo --> */}
+            {/* Logo */}
             <Link className='flex flex-shrink-0 items-center' href='/'>
               <Image
                 className='h-10 w-auto'
@@ -88,12 +88,11 @@ const Navbar = () => {
                 alt='AG Realty Logo'
                 priority={true}
               />
-
               <span className='hidden md:block text-white text-2xl font-bold ml-2'>
                 AG Realty
               </span>
             </Link>
-            {/* <!-- Desktop Menu Hidden below md screens --> */}
+            {/* Desktop Menu Hidden below md screens */}
             <div className='hidden md:ml-6 md:block'>
               <div className='flex space-x-2'>
                 <Link
@@ -126,7 +125,7 @@ const Navbar = () => {
             </div>
           </div>
 
-          {/* <!-- Right Side Menu (Logged Out) --> */}
+          {/* Right Side Menu (Logged Out) */}
           {!session && (
             <div className='hidden md:block md:ml-6'>
               <div className='flex items-center'>
@@ -161,7 +160,7 @@ const Navbar = () => {
             </div>
           )}
 
-          {/* <!-- Right Side Menu (Logged In) --> */}
+          {/* Right Side Menu (Logged In) */}
           {session && (
             <div className='absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0'>
               <Link href='/messages' className='relative group'>
@@ -175,7 +174,7 @@ const Navbar = () => {
                 </button>
                 <UnreadMessageCount session={session} />
               </Link>
-              {/* <!-- Profile dropdown button --> */}
+              {/* Profile dropdown button */}
               <div className='relative ml-3'>
                 <div>
                   <button
@@ -198,7 +197,7 @@ const Navbar = () => {
                   </button>
                 </div>
 
-                {/* <!-- Profile dropdown --> */}
+                {/* Profile dropdown */}
                 {isProfileMenuOpen && (
                   <div
                     id='user-menu'
@@ -252,15 +251,16 @@ const Navbar = () => {
         </div>
       </div>
 
-      {/* <!-- Mobile menu, show/hide based on menu state. --> */}
+      {/* Mobile Menu */}
       {isMobileMenuOpen && (
-        <div id='mobile-menu' className='md:hidden'>
-          <div className='space-y-1 px-2 pb-3 pt-2'>
+        <div className='md:hidden' id='mobile-menu'>
+          <div className='space-y-1 px-2 pt-2 pb-3 sm:px-3'>
             <Link
               href='/'
               className={`${
                 pathname === '/' ? 'bg-black' : ''
-              } text-white block rounded-md px-3 py-2 text-base font-medium`}
+              } text-white hover:bg-gray-900 hover:text-white block rounded-md px-3 py-2`}
+              onClick={() => setIsMobileMenuOpen(false)}
             >
               Home
             </Link>
@@ -268,7 +268,8 @@ const Navbar = () => {
               href='/properties'
               className={`${
                 pathname === '/properties' ? 'bg-black' : ''
-              } text-white block rounded-md px-3 py-2 text-base font-medium`}
+              } text-white hover:bg-gray-900 hover:text-white block rounded-md px-3 py-2`}
+              onClick={() => setIsMobileMenuOpen(false)}
             >
               Properties
             </Link>
@@ -277,39 +278,12 @@ const Navbar = () => {
                 href='/properties/add'
                 className={`${
                   pathname === '/properties/add' ? 'bg-black' : ''
-                } text-white block rounded-md px-3 py-2 text-base font-medium`}
+                } text-white hover:bg-gray-900 hover:text-white block rounded-md px-3 py-2`}
+                onClick={() => setIsMobileMenuOpen(false)}
               >
                 Add Property
               </Link>
             )}
-            {!session &&
-              providers &&
-              Object.values(providers).map((provider, index) => (
-                <button
-                  onClick={() => {
-                    if (provider.id === 'google') {
-                      signIn(provider.id);
-                    } else if (provider.id === 'credentials') {
-                      // Navigate to '/auth/login' page
-                      window.location.href = '/login';
-                    }
-                  }}
-                  key={index}
-                  className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 mr-2'
-                >
-                  {provider.id === 'google' ? (
-                    <>
-                      <FaGoogle className='text-white mr-2' />
-                      <span>Login/Register</span>
-                    </>
-                  ) : provider.id === 'credentials' ? (
-                    <>
-                      <FaEnvelope className='text-white mr-2' />
-                      <span>Login/Register</span>
-                    </>
-                  ) : null}
-                </button>
-              ))}
           </div>
         </div>
       )}
