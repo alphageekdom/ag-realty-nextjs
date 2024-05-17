efef47d461f0cf712613715e959ab51b73c504a0
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-05-17 09:27:30 -0700

    fixed getSessionUser

--- config/database.js
+++ config/database.js
@@ -12,14 +12,9 @@ const connectDB = async () => {
   }
 
   //   Connect to MongoDB
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    connected = true;
-    console.log('MongoDB connected...');
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+  await mongoose.connect(process.env.MONGODB_URI);
+  connected = true;
+  console.log('MongoDB connected...');
 };
 
 export default connectDB;
