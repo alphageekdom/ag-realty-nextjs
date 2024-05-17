6e6fa695346452a6f0f2ce441eaedb2db9d8ce87
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-05-16 19:11:52 -0700

    database.js fix

--- config/database.js
+++ config/database.js
@@ -15,7 +15,6 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     connected = true;
     console.log('MongoDB connected...');
