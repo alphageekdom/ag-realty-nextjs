f3e154c4768ff5c53174a47c9f1b04c04fb4656d
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-05-17 09:07:47 -0700

    push

--- utils/requests.js
+++ utils/requests.js
@@ -10,7 +10,11 @@ async function fetchProperties({ showFeatured = false } = {}) {
 
     const res = await fetch(
       `${apiDomain}/properties${showFeatured ? '/featured' : ''}`,
-      { cache: 'no-store' }
+      {
+        headers: {
+          'Cache-Control': 'no-store',
+        },
+      }
     );
 
     if (!res.ok) {
