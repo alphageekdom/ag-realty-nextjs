d76f2c57939f6dc7814ac54a6b6690ff1ae2592a
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-05-16 18:18:50 -0700

    fixed search endpoint

--- app/api/properties/search/route.js
+++ app/api/properties/search/route.js
@@ -2,29 +2,32 @@ import connectDB from '@/config/database';
 import Property from '@/models/Property';
 
 // GET /api/properties/search
-export const GET = async (request) => {
+export const GET = async (req) => {
   try {
     await connectDB();
 
-    const { searchParams } = new URL(request.url);
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const searchParams = url.searchParams;
     const location = searchParams.get('location');
     const propertyType = searchParams.get('propertyType');
 
-    const locationPattern = new RegExp(location, 'i');
+    // Construct the query object
+    let query = {};
 
-    // Match location pattern against database fields
-    let query = {
-      $or: [
+    // Add location to query if provided
+    if (location) {
+      const locationPattern = new RegExp(location, 'i');
+      query.$or = [
         { name: locationPattern },
         { description: locationPattern },
         { 'location.street': locationPattern },
         { 'location.city': locationPattern },
         { 'location.state': locationPattern },
         { 'location.zipcode': locationPattern },
-      ],
-    };
+      ];
+    }
 
-    // Only check for property if it's not 'All'
+    // Add property type to query if provided and not 'All'
     if (propertyType && propertyType !== 'All') {
       const typePattern = new RegExp(propertyType, 'i');
       query.type = typePattern;
@@ -34,7 +37,7 @@ export const GET = async (request) => {
 
     return new Response(JSON.stringify(properties), { status: 200 });
   } catch (error) {
-    console.log(error);
+    console.error('Error fetching properties:', error);
     return new Response('Something Went Wrong', { status: 500 });
   }
 };
